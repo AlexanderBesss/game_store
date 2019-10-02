9c308e260f19f7374c7910abb15a36f5cc271dfa
Alexander Besarab <alexandrbesss@gmail.com>
2019-10-02 15:33:21 +0300

    remove test files

--- src/modules/api/game/pipes/param-id-validation.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ParamIdValidationPipe } from './param-id-validation.pipe';
-
-describe('ParamIdValidationPipe', () => {
-  it('should be defined', () => {
-    expect(new ParamIdValidationPipe()).toBeDefined();
-  });
-});
