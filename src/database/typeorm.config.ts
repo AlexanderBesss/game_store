0e51bdbfb8eb05af7562864ac61f58effb2205bc
Alexander Besarab <alexandrbesss@gmail.com>
2019-10-02 16:04:00 +0300

    add readme

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -2,11 +2,8 @@ import { ConfigService } from '../modules/config/services/config.service';
 import { Game } from '../shared/entities/game.entity';
 import { Publisher } from '../shared/entities/publisher.entity';
 const config = new ConfigService();
-const entitiesPath =
-  process.env.NODE_ENV === 'production'
-    ? 'dist/shared/entities/**.entity{.ts,.js}'
-    : 'src/shared/entities/**.entity{.ts,.js}';
 const migrationDir = process.env.NODE_ENV === 'production' ? 'dist/database/migrations' : 'src/database/migrations';
+
 export = {
   host: config.DB_HOST,
   type: 'mysql',
